c82f560c7f1092f754ff6a17ad23fdf311cccebe
zzfbwoals <ryumin0421@gmail.com>
2026-08-12 15:11:34 +0900

    [init] 리액트·스프링부트 프로젝트 세팅

---
+++ Homework4/be/kms/src/main/java/com/ineb/kms/KmsApplication.java
@@ -0,0 +1,13 @@
+package com.ineb.kms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmsApplication.class, args);
+	}
+
+}
