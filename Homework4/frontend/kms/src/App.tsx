95e4208d4ea04b380ce5f241da62460ef9bc4808
zzfbwoals <ryumin0421@gmail.com>
2026-08-13 15:16:13 +0900

    [rename] 폴더 명 변경 및 프로젝트 재 생성

--- Homework4/frontend/kms/src/App.tsx
+++ Homework4/frontend/kms/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
